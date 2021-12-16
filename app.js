04ec3bfe09d17c4289f02a29dd66ba0ce475eeec
Vance McGrady <vancemcgrady@gmail.com>
2021-12-16 12:49:09 -0700

    grid is positioned and sized correctly

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("testing");
+function createGrid() {
+  const mainContainer = document.querySelector(".main-container");
+
+  for (let i = 0; i < 16; i++) {
+    const unit = document.createElement("div");
+    unit.classList.add("unit");
+    mainContainer.appendChild(unit);
+  }
+}
+
+createGrid();
