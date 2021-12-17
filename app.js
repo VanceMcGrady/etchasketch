b7489b1376ba187b1ea7ca8a5ea019d3253eee04
Vance McGrady <vancemcgrady@gmail.com>
2021-12-17 10:27:06 -0700

    Got etch-a-sketch screen working. No other features yet.

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 function createGrid() {
   const mainContainer = document.querySelector(".main-container");
 
-  for (let i = 0; i < 16; i++) {
-    const unit = document.createElement("div");
-    unit.classList.add("unit");
-    mainContainer.appendChild(unit);
+  for (let i = 0; i < 30000; i++) {
+    const newUnit = document.createElement("div");
+    newUnit.classList.add("unit");
+    mainContainer.appendChild(newUnit);
+    newUnit.addEventListener(
+      "mouseenter",
+      function (e) {
+        e.target.style.backgroundColor = "black";
+      },
+      { capture: false, once: true }
+    );
   }
 }
 
 createGrid();
+
+const unit = document.querySelector(".unit");
