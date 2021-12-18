8e77c2ee95b21aa004663e18a7216cc0d3ca4e56
Vance McGrady <vancemcgrady@gmail.com>
2021-12-18 12:01:13 -0700

    redesigned grid is working.

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-const padContainer = document.querySelector(".pad-container");
-const clearBtn = document.querySelector(".clear-btn");
-const slider = document.querySelector(".slider");
+const container = document.querySelector(".container");
 
-createGrid();
-
-const unit = document.querySelector(".unit");
-
-slider.addEventListener("input", function (e) {
-  unit.style.width = `${slider.value}px`;
-  unit.style.height = `${slider.value}px`;
-});
-
-function createGrid() {
-  for (let i = 0; i < 10000; i++) {
-    const newUnit = document.createElement("div");
-    newUnit.classList.add("unit");
-    padContainer.appendChild(newUnit);
-    newUnit.addEventListener(
-      "mouseenter",
-      function (e) {
-        e.target.classList.toggle("active");
-      },
-      { capture: false, once: true }
-    );
+function createGrid(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    container.appendChild(cell).className = "grid-item";
   }
 }
 
-clearBtn.addEventListener("click", function clearPad() {
-  padContainer.innerHTML = "";
-  createGrid();
-});
+createGrid(16, 16);
