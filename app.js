a048bfc79c03e7ffa3d96eb146864d106b823879
Vance McGrady <vancemcgrady@gmail.com>
2021-12-18 12:09:20 -0700

    red border okay for now.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ function createGrid(rows, cols) {
   container.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.innerText = c + 1;
+    cell.addEventListener("mouseenter", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
     container.appendChild(cell).className = "grid-item";
   }
 }
